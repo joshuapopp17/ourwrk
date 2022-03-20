f11607b2ee0d0313ff6930b82ef3712065d6f5c9
joshuapopp17 <joshuapopp17@csu.fullerton.edu>
2022-03-19 23:11:01 -0700

    for u bb

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div class="d-flex p-2 justify-content-center align-items-center">
+    <div class="d-flex flex-column p-2 justify-content-center align-items-center h-100">
       <Button type="button" className="btn btn-primary">Button #1</Button>
+      <Button type="button" className="btn btn-success">Button #2</Button>
+      <Button type="button" className="btn btn-info">Button #3</Button>
     </div>
   );
 }
