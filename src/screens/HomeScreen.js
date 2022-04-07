6a5e384c06691c060ef8d792daf2611400f9b0c3
joshuapopp17 <joshuapopp17@csu.fullerton.edu>
2022-04-07 15:10:39 -0700

    Finished Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 import './HomeScreen.css'
 
 function HomeScreen() {
   return (
     <div className='bg'>
-      <div className={'homeContainer'} style={{paddingTop: '70px'}}>
+      <div className={'homeContainer'}>
         <Container className={'box'}>
-          <h1>OurWrk</h1>
-          <h3>bringing communities togehter one hour at a time</h3>
+          <h1 className={'title'}>Welcome to OurWrk</h1>
+          <h3 className={'text'}>bringing communities togehter one hour at a time</h3>
+          <Container className={'buttonContainer'}>
+            <Button className={'button'} variant="primary" type="submit" href={"/login"}>
+                Log In
+            </Button>
+            <Button className={'button'} variant="outline-dark" type="submit" href={"/signup"}>
+                Don't have an account? Sign Up
+            </Button>
+          </Container>
         </Container>
       </div>
     </div>
