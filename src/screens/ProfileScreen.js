9e9713da0e769252955ad8239fa05fffd46eadac
JoahanEspinosa <espinosa_joahanxn@csu.fullerton.edu>
2022-04-15 01:25:24 -0700

    Joahan Made the ProfileScreen usable

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,24 @@
-import React, { useContext } from 'react'
-import { Button, Container } from 'react-bootstrap'
-import { UserContext } from '../App';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import ListingContainer from '../components/ListingContainer.js'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './ProfileScreen.css'
 
 function ProfileScreen() {
   return (
-      <Container style={{paddingTop: '70px'}} className={'mt-5'}>
-            <div>ProfileScreen</div>
+      <Container style={{paddingTop: '130px'}}>
+          <div className={'roundContainer1'}>
+            <Row mb={1}>
+                <Col xs={1}><img src="http://www.killersites.com/blog/wp-content/uploads/2012/04/icon-html.png" alt="..." class="img-fluid" style={{maxHeight: '100%'}}></img></Col>
+                <Col class="border border-2" className={'textWrap'} md={{offset: 1}}>Username</Col>
+            </Row>
+          </div>
+          
+            <Row>
+              <Col className={'roundContainer2'}><ListingContainer category={"Gardening"}/></Col>
+              <Col className={'roundContainer3'} md={{span:4, offset: 1}}>Hours Planned: <br></br> 4 <br></br> <br></br>Hours Served: <br></br> 4</Col>
+            </Row>
       </Container>
   )
 }
